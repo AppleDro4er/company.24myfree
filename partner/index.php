15efa8d47673b284244f3969dcb34095c96d8938
AppleDro4er <pooloz7@gmail.com>
2017-07-28 18:38:40 +0300

    start project

---
+++ partner/index.php
@@ -0,0 +1,265 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>«Рекламное агенство "ННК"»</title>
+<link href="/css/style.css" media="all" rel="stylesheet">
+<link href="/css/fawesome.css" media="all" rel="stylesheet">
+<link href="/css/uikit.min.css" media="all" rel="stylesheet">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<script src="/js/jquery.min.js"></script>
+<script src="/js/uikit.min.js"></script>
+<!-- plugins uikit -->
+<script src="/js/components/slideshow.min.js"></script>
+<script src="/js/components/slideshow-fx.min.js"></script>
+<script src="/js/components/sticky.min.js"></script>
+<script src="/js/core/nav.min.js"></script>
+<script src="/js/core/core.min.js"></script>
+<script src="/js/core/scrollspy.min.js"></script>
+</head>
+<body class="tm-navbar-fixed">
+<main id="page">
+<div class="tm-headerbar uk-clearfix">
+<div class="uk-container uk-container-center">
+<div class="uk-navbar-nav uk-navbar-flip uk-hidden-small">
+<ul class="uk-navbar-nav uk-hidden-small">
+<li><a href="/">Главная</a></li>
+<li class="uk-parent" data-uk-dropdown="{}"><a href="/services/">Услуги</a>
+<div class="uk-dropdown uk-dropdown-navbar uk-dropdown-width-1 uk-dropdown-bottom" style="top: 90px; left: 0px;">
+<div class="uk-grid uk-dropdown-grid">
+<div class="uk-width-1-1">
+<ul class="uk-nav uk-nav-navbar">
+<li><a href="/">SEO / SMO</a></li>
+<li><a href="/">Создание сайтов</a></li>
+<li><a href="/">Продвижение сайтов</a></li>
+<li><a href="/">Организация выставок</a></li>
+<li><a href="/">Продюсирование</a></li>
+<li><a href="/">Консультации</a></li>
+<li><a href="/">Реклама в СМИ</a></li>
+</ul>
+</div>
+</div>
+</div>
+</li>
+<li class="uk-active"><a href="/partner/">Партнерам</a></li>
+<li><a href="/vacancy/">Вакансии </a></li>
+</ul>
+</div>
+</div>
+</div>
+<header>
+<div id="title">Партнерам рекламного агенства "ННК"</div>
+</header>
+
+<news data-uk-grid-margin="" data-uk-scrollspy="{cls:'uk-animation-fade uk-invisible', target:'.news', delay:300}">
+
+<div class="news uk-invisible">
+<div class="img-bg-wrap bg-cropping" style="background-image: url('/img/services/seo.jpg');">
+<div class="label"><div class="read">Подробнее</div></div>
+<div class="text">SEO / SMO</div></div>
+</div>
+
+<div class="news uk-invisible">
+<div class="img-bg-wrap bg-cropping" style="background-image: url('/img/services/site.jpg');">
+<div class="label"><div class="read">Подробнее</div></div>
+<div class="text">Создание / Продвижение сайтов</div></div>
+</div>
+
+<div class="news uk-invisible">
+<div class="img-bg-wrap bg-cropping" style="background-image: url('/img/services/org.jpg');">
+<div class="label"><div class="read">Подробнее</div></div>
+<div class="text">Организация выставок</div></div>
+</div>
+
+<div class="news uk-invisible">
+<div class="img-bg-wrap bg-cropping" style="background-image: url('/img/services/prod.jpg');">
+<div class="label"><div class="read">Подробнее</div></div>
+<div class="text">Продюсирование</div></div>
+</div>
+
+<div class="news uk-invisible">
+<div class="img-bg-wrap bg-cropping" style="background-image: url('/img/services/cons.jpg');">
+<div class="label"><div class="read">Подробнее</div></div>
+<div class="text">Консультации</div></div>
+</div>
+
+<div class="news uk-invisible">
+<div class="img-bg-wrap bg-cropping" style="background-image: url('/img/services/smi.jpg');">
+<div class="label"><div class="read">Подробнее</div></div>
+<div class="text">Реклама в СМИ</div></div>
+</div>
+</news>
+
+
+<div class="tm-teaser">
+<div class="uk-slidenav-position" data-uk-slideshow="{autoplay:true}">
+<ul class="uk-slideshow uk-overlay-active">
+<li>
+<div class="uk-cover-background uk-position-cover" style="background-image: url(/img/slide/1.jpg);"></div>
+<canvas width="1170" height="250" style="width: 100%; height: auto; opacity: 0;"></canvas>
+<div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-flex uk-flex-center uk-flex-middle uk-text-center">
+<div>
+<h3 class="uk-h2">
+Стремясь к лучшему
+</h3>
+<p>Компания постоянно развивается</p><!--<a class="uk-button uk-button-primary" href="/">Написать нам</a>-->
+</div>
+</div>
+</li>
+<li>
+<div class="uk-cover-background uk-position-cover" style="background-image: url(/img/slide/2.jpg);"></div>
+<canvas width="1170" height="250" style="width: 100%; height: auto; opacity: 0;"></canvas>
+<div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-flex uk-flex-center uk-flex-middle uk-text-center">
+<div>
+<h3 class="uk-h2">
+Быстрее только свет
+</h3>
+<p>Выполним любой сложности заказ в срок</p><!--<a class="uk-button uk-button-primary" href="/">Написать нам</a>-->
+</div>
+</div>
+</li>
+<li>
+<div class="uk-cover-background uk-position-cover" style="background-image: url(/img/slide/3.jpg);"></div>
+<canvas width="1170" height="250" style="width: 100%; height: auto; opacity: 0;"></canvas>
+<div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-flex uk-flex-center uk-flex-middle uk-text-center">
+<div>
+<h3 class="uk-h2">
+Нам можно доверять
+</h3>
+<p>И нужно</p><!--<a class="uk-button uk-button-primary" href="/">Написать нам</a>-->
+</div>
+</div>
+</li>
+<li>
+<div class="uk-cover-background uk-position-cover" style="background-image: url(/img/slide/4.jpg);"></div>
+<canvas width="1170" height="250" style="width: 100%; height: auto; opacity: 0;"></canvas>
+<div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-flex uk-flex-center uk-flex-middle uk-text-center">
+<div>
+<h3 class="uk-h2">
+Мы распространняем только
+</h3>
+<p>Иновационные идеи</p><!--<a class="uk-button uk-button-primary" href="/">Написать нам</a>-->
+</div>
+</div>
+</li>
+</ul>
+<a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slideshow-item="previous"></a>
+<a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slideshow-item="next"></a>
+</div>
+</div>
+
+
+<?php
+if(isset($_POST['submit'])) {
+if(trim($_POST['name']) == '') {
+$hasError = true;
+$message = "незаполненного имени";
+} else {
+$name = trim($_POST['name']);
+if(trim($_POST['email']) == ''){
+$hasError = true;
+$message = "незаполненного e-mail";
+} else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim($_POST['email']))) {
+$hasError = true;
+$message = "некорректного e-mail";
+} else {
+$email = trim($_POST['email']);
+if(trim($_POST['subject']) == '') {
+$hasError = true;
+$message = "незаполненной темы сообщения";
+} else {
+$subject = trim($_POST['subject']);
+if(trim($_POST['message']) == '') {
+$hasError = true;
+$message = "незаполненного сообщения";
+} else {
+if(function_exists('stripslashes')) {
+$comments = stripslashes(trim($_POST['message']));
+} else {
+$comments = trim($_POST['message']);
+}}}}}
+if(!isset($hasError)) {
+$to = 'pooloz7@gmail.com';
+$body = "$comments";
+$headers =
+"From: $name <$email>\r\n".
+"Content-type: text/html; charset=utf-8"."\r\n";
+mail($to, $subject, $body, $headers);
+$emailSent = true;
+echo '<div class="yahoo">'.$name.' Сообщение отправленно</div>';
+} else {
+echo '<div class="oh-shit">'.$name.' Сообщение не отправленно из-за '.$message.'</div>';
+}}
+?>
+<div class="callback">
+<h1>Напишите нам</h1>
+<div class="calback-body">
+<form method="post" action="#" id="contact-form">
+<ul>
+<li class="field"><input class="input" type="text" placeholder="Ваше Имя" name="name" id="your-name"></li>
+<li class="field"><input class="input" type="email" placeholder="Ваш Email" name="email" id="your-email"></li>
+<li class="field"><input class="input" type="text" placeholder="Тема" name="subject" id="your-subject"></li>
+<li class="field"><textarea class="input textarea" placeholder="Ваше сообщение" rows="4" name="message" id="your-message"></textarea></li></ul>
+<div class="btn">
+<button href="" type="submit" name="submit" value="Отправить"><span>Отправить</span></button>
+</div>
+</form></div>
+</div>
+<script>
+var neighborhoods = [
+{lat: 59.92, lng: 30.31},
+{lat: 59.94, lng: 30.30},
+{lat: 59.93, lng: 30.34},
+{lat: 59.95, lng: 30.38}
+];
+var markers = [];
+var map;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+zoom: 12,
+center: {lat: 59.93, lng: 30.33}
+});
+}
+$(document).ready(function() {
+clearMarkers();
+for (var i = 0; i < neighborhoods.length; i++) {
+addMarkerWithTimeout(neighborhoods[i], i * 200);
+}
+});
+
+function addMarkerWithTimeout(position, timeout) {
+window.setTimeout(function() {
+markers.push(new google.maps.Marker({
+position: position,
+map: map
+}));
+}, timeout);
+}
+
+function clearMarkers() {
+for (var i = 0; i < markers.length; i++) {
+markers[i].setMap(null);
+}
+markers = [];
+}
+</script>
+<div id="map"><script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqSWZf2uDH9-9lLkNyM3Y94iEK3TJKGok&callback=initMap" type="text/javascript"></script></div>
+
+<footer>
+<div class="footerItem">
+<div class="footerTitle"><b>Мы в социальных сетях</b></div>
+<ul class="social-icon-list">
+<li><a href=""><i class="fa fa-facebook"></i></a></li>
+<li><a href=""><i class="fa fa-twitter"></i></a></li>
+<li><a href=""><i class="fa fa-vk"></i></a></li>
+<li><a href=""><i class="fa fa-google-plus"></i></a></li>
+<li><a href=""><i class="fa fa-youtube"></i></a></li>
+</ul>
+</div>
+</footer>
+<div class="item">
+AppleDro4er © 2016
+</div>
+</main>
+</body>
+</html>
